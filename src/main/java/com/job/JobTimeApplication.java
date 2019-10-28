6b10d0dbd0db658a2b95992f40333e61e82cf753
keith <G-keith>
2019-10-28 09:10:49 +0800

    第一次提交

---
+++ src/main/java/com/job/JobTimeApplication.java
@@ -0,0 +1,13 @@
+package com.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobTimeApplication.class, args);
+    }
+
+}
