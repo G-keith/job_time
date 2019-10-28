b76fcc3b48c7353c6a4fa895648a17531a0047ba
keith <G-keith>
2019-10-28 10:17:28 +0800

    增加swagger

---
+++ src/main/java/com/job/common/config/Swagger2.java
@@ -0,0 +1,62 @@
+package com.job.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author
+ * @version
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+    /**
+     * swagger2的配置文件，这里可以配置swagger2的一些基本的内容，比如扫描的包等等
+     * @return
+     */
+    @Bean
+    public Docket createRestApi() {
+        ParameterBuilder ticketPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<Parameter>();
+        ticketPar.name("token").description("添加token参数")
+                .modelRef(new ModelRef("string")).parameterType("header")
+                .required(false).build(); //header中的ticket参数非必填，传空也可以
+        pars.add(ticketPar.build());    //根据每个方法名也知道当前方法在设置什么参数
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.job.controller"))
+                .build()
+                .globalOperationParameters(pars)
+                .apiInfo(apiInfo());
+    }
+    /**
+     * 构建 api文档的详细信息函数,注意这里的注解引用的是哪个
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                //页面标题
+                .title("CIS - 注塑行业 API")
+                //创建人
+                .contact(new Contact("CIS", "http://www.cisinfo.cn", "mail@cisinfo.cn"))
+                //版本号
+                .version("1.0")
+                //描述
+                .description("CIS: API描述")
+                .build();
+    }
+}
