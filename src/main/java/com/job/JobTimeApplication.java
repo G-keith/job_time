c32d46efa2b640f8d61e57efce129bc88f7a11dd
keith <G-keith>
2019-10-28 09:29:30 +0800

    测试

--- src/main/java/com/job/JobTimeApplication.java
+++ src/main/java/com/job/JobTimeApplication.java
@@ -2,7 +2,11 @@ package com.job;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * @author keith
+ * @version 1.0
+ * @date 2019-10-28
+ */
 @SpringBootApplication
 public class JobTimeApplication {
 
